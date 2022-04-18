a1342942cbeb3151e319126fd3656004c4bd72b5
Marco <marco.beer93@web.de>
2022-04-18 17:29:20 +0200

    repaired the win-lines

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let fields = [];
-
+let gameOver = false;
 let currentShape = 'cross';
 
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
         if (currentShape == 'cross') {
             currentShape = 'circle';
             document.getElementById('player-1').classList.add('player-inactive')
@@ -55,7 +55,7 @@ function checkForWin() {
         document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1)'
     }
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
-        winner = fields[2];
+        winner = fields[1];
         document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)'
     }
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
@@ -68,11 +68,31 @@ function checkForWin() {
         document.getElementById('line-6').style.transform = 'rotate(45deg)scaleX(1)'
     }
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
-        winner = fields[1];
+        winner = fields[2];
         document.getElementById('line-7').style.transform = ' rotate(-45deg)scaleX(1)'
     }
 
     if (winner) {
         console.log('Gewonnen', winner)
+        gameOver = true;
+        setTimeout(function() {
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-btn').classList.remove('d-none');
+        }, 1500)
     }
+}
+
+function restartGame() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+    for (let i = 0; i < 8; i++) {
+        document.getElementById('line-' + i).classList.add('d-none');
+    }
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('circle-' + i).classList.add('d-none');
+        document.getElementById('cross-' + i).classList.add('d-none');
+    }
+
 }
